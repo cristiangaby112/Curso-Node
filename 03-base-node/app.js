8c166569637e3a6da3e6467da8b5070353d53caf
cristiangaby112 <cristiangabrielbarriento@outlook.com>
2022-04-11 14:05:01 -0300

    03-base-node

---
+++ 03-base-node/app.js
@@ -0,0 +1,11 @@
+
+const base = 5;
+for (let i = 1; i <= 10; i++) {
+  let result = i * 5
+  console.log(`${base} X ${i} = ${result}`)
+}
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let multiplicar = array.map(m => (m * 5))
+
+// console.log(multiplicar)
